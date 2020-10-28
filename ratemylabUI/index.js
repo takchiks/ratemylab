5e2671dbd652c23080cf2b5541f0d40993693409
Takchiks@gmail.com <Takchiks@gmail.com>
2020-10-28 01:39:14 -0400

    sample ejs node js

--- ratemylabUI/index.js
+++ ratemylabUI/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const fs = require("fs");
+
 app.use('/css', express.static('css'));
+app.use('/img', express.static('img'));
+app.use('/bootstrapcss', express.static('bootstrap/css'));
+
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:false}));
 
@@ -14,5 +19,20 @@ app.get("/users",(req,res) =>{
 
 app.get("/test/:id", (req,res)=>{
     res.render("profile", {id:req.params.id});
+});
+app.get("/index", (req,res)=>{
+    res.render("index");
+});
+app.get("/change_pw", (req,res)=>{
+    res.render("change_pw");
+});
+app.get("/new_lab", (req,res)=>{
+    res.render("new_lab");
+});
+app.get("/prev_lab", (req,res)=>{
+    res.render("prev_lab");
+});
+app.post("/login", (req,res)=>{
+    res.render("home");
 });
 app.listen(3000);
