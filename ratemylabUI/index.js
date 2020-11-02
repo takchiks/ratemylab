02d8654abbe298c992d5ca84d47d2257f36a4014
Takchiks@gmail.com <Takchiks@gmail.com>
2020-11-02 02:13:11 -0500

    Matching Wireframes UI

--- ratemylabUI/index.js
+++ ratemylabUI/index.js
@@ -8,7 +8,7 @@ app.use('/img', express.static('img'));
 app.use('/bootstrapcss', express.static('bootstrap/css'));
 
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.set("view engine", "ejs");
@@ -34,7 +34,37 @@ app.get("/prev_lab", (req,res)=>{
 app.get("/home", (req,res)=>{
     res.render("home");
 });
+app.get("/", (req,res)=>{
+    res.render("index");
+});
 app.post("/login", (req,res)=>{
+    res.render("initial_pw");
+});
+app.post("/reset", (req,res)=>{
     res.render("home");
+});
+app.get("/config_lab", (req,res)=>{
+    res.render("config_lab");
+});
+app.get("/view_rating", (req,res)=>{
+    res.render("view_rating");
+});
+app.post("/view_lab_ratings", (req,res)=>{
+    res.render("view_lab_ratings");
+});
+app.get("/view_lab_ratings", (req,res)=>{
+    res.render("view_lab_ratings");
+});
+app.post("/view_detailed", (req,res)=>{
+    res.render("view_detailed");
+});
+app.post("/config_lab_post", (req,res)=>{
+    res.render("config_lab");
+});
+app.post("/choose_labs", (req,res)=>{
+    res.render("choose_labs");
+});
+app.post("/initial", (req,res)=>{
+    res.render("initial_pw");
 });
 app.listen(3000);
